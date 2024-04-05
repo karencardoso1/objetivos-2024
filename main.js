3b200e1d71ad92fbef1c2ca81a3dda67c2fc4434
karencardoso1 <151936876+karencardoso1@users.noreply.github.com>
2024-04-05 11:44:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-const botoes = querySelectorAll(".botao");
-
+const botoes = document.querySelectorAll(".botao");
 for(let i= 0; i< botoes.length; i++){
     botoes[i].onclick = function(){
-        for (j=0; j< botoes.length; j++){
-            botoes[j].classlist.remove("ativo");            
+        for(j=0; j<botoes.length; j++){
+            botoes[j].classList.remove("ativo");
         }
-        botoes[i].classlist.add("ativo");
+        botoes[i].classList.add("ativo");
     }
 }
